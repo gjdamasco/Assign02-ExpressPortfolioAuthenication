063a79a90516a5d1d7bc53c4fbbd5f683cc06e28
gjdamasco <gjcenina@gmail.com>
2021-10-24 22:21:44 -0400

    updated db URI to mongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
-// db.js file for the  URI connection configuration for the local mongoDB
+// db.js file for the  URI connection configuration for the mongoDB
 module.exports =
 {
-    "URI": "mongodb://localhost/contacts" 
+    //"URI": "mongodb://localhost/contacts"
+    "URI": "mongodb+srv://geejay:6wryCEitfUsNc4hK@geejay.6mjhr.mongodb.net/contact_list?retryWrites=true&w=majority"
 }
